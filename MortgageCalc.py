6227cde1668719b10fb66db7c0e98d8aca805c9f
Ziyad <zdn313@yahoo.com>
2022-08-11 11:05:29 -0500

    n/a

--- MortgageCalc.py
+++ MortgageCalc.py
@@ -7,19 +7,8 @@ __      __      _
  \ \/\/ / / -_)| |/ _|/ _ \| '  \ / -_)
   \_/\_/  \___||_|\__|\___/|_|_|_|\___|
 ----------------------------------------
-        Floor Plan Calculator!
+        Mortgage Calculator!
 
 
 ''')
 
-
-
-userlen = float(input('Please enter the length of the floor you wish to cover (sq.ft): '))
-userwidth = float(input('Please enter the width of the floor you wish to cover (sq.ft): '))
-userprice = float(input('Please enter the price of flooring per square foot: '))
-
-area = userlen * userwidth
-price = userprice * area
-messege = ('The total cost of the flooring is ${}' )
-
-print(messege.format(price))
