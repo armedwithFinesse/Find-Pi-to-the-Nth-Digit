338b2aa40ae1fe0846b4ce3bb44327f5cbeef9aa
Ziyad <zdn313@yahoo.com>
2022-08-12 14:39:09 -0500

    cleaned up conditionals into functions, added minor error handling

--- SpellingNumbers.py
+++ SpellingNumbers.py
@@ -18,178 +18,88 @@ Then I have to define the place value of any number up to and including 1,000,00
   
   '''
 
+def singledigitplacevalue(placevalue):
 
+      for i in user_input_number:
+        if i == '1':
+            value = 'One' 
+        elif i == '2':
+            value = 'Two'
+        elif i == '3':
+            value = 'Three'
+        elif i == '4':
+            value = 'Four'
+        elif i == '5':
+            value = 'Five'
+        elif i == '6':
+            value = 'Six'
+        elif i == '7':
+            value = 'Seven'
+        elif i == '8':
+            value = 'Eight'
+        elif i == '9':
+            value = 'Nine'
+        if placevalue == None:
+            print(value)
+            break
+        else:
+            print(value + placevalue)  
+            break 
+def doubledigitplacevalue(placevalue):
+    for i in user_input_number:
+        if i == '1':
+            value = 'Ten' 
+        elif i == '2':
+            value = 'Twenty'
+        elif i == '3':
+            value = 'Thirty'
+        elif i == '4':
+            value = 'Fourty'
+        elif i == '5':
+            value = 'Fifty'
+        elif i == '6':
+            value = 'Sixty'
+        elif i == '7':
+            value = 'Seventy'
+        elif i == '8':
+            value = 'Eighty'
+        elif i == '9':
+            value = 'Ninety'
+        if placevalue == None:
+            print(value)
+            break
+        else:
+            print(value + placevalue)  
+            break 
 
 begtup = ('m','ht','tt','t','h','t','o')
 
+while True:
+    try:
+        user_input_number = int(input('Enter a number between 0 and 1000000: '))
+        break
+    except:
+        print('Please enter a valid integer between 0 and 1000000')
+        continue
+    
 
-user_input_number = input('Enter a number between 0 and 1000000: ')
+user_input_number = str(user_input_number)
 user_input_number = tuple(user_input_number)
 if len(user_input_number) == 7: #millions
-    placevalue = ' Million'
-    for i in user_input_number:
-        if i == '1':
-            value = 'One' 
-        elif i == '2':
-            value = 'Two'
-        elif i == '3':
-            value = 'Three'
-        elif i == '4':
-            value = 'Four'
-        elif i == '5':
-            value = 'Five'
-        elif i == '6':
-            value = 'Six'
-        elif i == '7':
-            value = 'Seven'
-        elif i == '8':
-            value = 'Eight'
-        elif i == '9':
-            value = 'Nine'
-        print(value + placevalue)  
-        break 
+    singledigitplacevalue(' Million')
 elif len(user_input_number) == 6: # hundred -thousands
-    placevalue = ' Hundred-Thousand'
-    for i in user_input_number:
-        if i == '1':
-            value = 'One' 
-        elif i == '2':
-            value = 'Two'
-        elif i == '3':
-            value = 'Three'
-        elif i == '4':
-            value = 'Four'
-        elif i == '5':
-            value = 'Five'
-        elif i == '6':
-            value = 'Six'
-        elif i == '7':
-            value = 'Seven'
-        elif i == '8':
-            value = 'Eight'
-        elif i == '9':
-            value = 'Nine'
-        print(value + placevalue)  
-        break 
+    singledigitplacevalue(' Hundred-Thousand')
 elif len(user_input_number) == 5: #ten-thousands
-    placevalue = '-Thousand'
-    for i in user_input_number:
-        if i == '1':
-            value = 'Ten' 
-        elif i == '2':
-            value = 'Twenty'
-        elif i == '3':
-            value = 'Thirty'
-        elif i == '4':
-            value = 'Fourty'
-        elif i == '5':
-            value = 'Fifty'
-        elif i == '6':
-            value = 'Sixty'
-        elif i == '7':
-            value = 'Seventy'
-        elif i == '8':
-            value = 'Eighty'
-        elif i == '9':
-            value = 'Ninety'
-        print(value + placevalue)  
-        break 
-
-    
+    doubledigitplacevalue('-Thousand')
 elif len(user_input_number) == 4: #thousands
-    placevalue = ' Thousand'
-    for i in user_input_number:
-        if i == '1':
-            value = 'One' 
-        elif i == '2':
-            value = 'Two'
-        elif i == '3':
-            value = 'Three'
-        elif i == '4':
-            value = 'Four'
-        elif i == '5':
-            value = 'Five'
-        elif i == '6':
-            value = 'Six'
-        elif i == '7':
-            value = 'Seven'
-        elif i == '8':
-            value = 'Eight'
-        elif i == '9':
-            value = 'Nine'
-        print(value + placevalue)  
-        break 
+    singledigitplacevalue(' Thousand') 
 elif len(user_input_number) == 3: #hundreds
-    placevalue = ' Hundred'
-    for i in user_input_number:
-        if i == '1':
-            value = 'One' 
-        elif i == '2':
-            value = 'Two'
-        elif i == '3':
-            value = 'Three'
-        elif i == '4':
-            value = 'Four'
-        elif i == '5':
-            value = 'Five'
-        elif i == '6':
-            value = 'Six'
-        elif i == '7':
-            value = 'Seven'
-        elif i == '8':
-            value = 'Eight'
-        elif i == '9':
-            value = 'Nine'
-        print(value + placevalue)  
-        break  
+    singledigitplacevalue(' Hundred') 
 elif len(user_input_number) == 2: #tens
-    for i in user_input_number:
-        if i == '1':
-            value = 'Ten' 
-        elif i == '2':
-            value = 'Twenty'
-        elif i == '3':
-            value = 'Thirty'
-        elif i == '4':
-            value = 'Fourty'
-        elif i == '5':
-            value = 'Fifty'
-        elif i == '6':
-            value = 'Sixty'
-        elif i == '7':
-            value = 'Seventy'
-        elif i == '8':
-            value = 'Eighty'
-        elif i == '9':
-            value = 'Ninety'
-        print(value)  
-        break 
+    doubledigitplacevalue(None)
 elif len(user_input_number) == 1: #ones
-    #placevalue = ' '
-    for i in user_input_number:
-        if i == '1':
-            value = 'One' 
-        elif i == '2':
-            value = 'Two'
-        elif i == '3':
-            value = 'Three'
-        elif i == '4':
-            value = 'Four'
-        elif i == '5':
-            value = 'Five'
-        elif i == '6':
-            value = 'Six'
-        elif i == '7':
-            value = 'Seven'
-        elif i == '8':
-            value = 'Eight'
-        elif i == '9':
-            value = 'Nine'
-        print(value)  
-        break 
+    singledigitplacevalue(None)
 
-else:
-    print('need to add one to len of user input')
 
 
 
