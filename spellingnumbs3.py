5ed7c93d77cb6a7c9ce978924fdd6436d5e47f10
Ziyad <zdn313@yahoo.com>
2022-08-15 12:25:02 -0500

    found bugs and fixed them. Fixed most formatting issues too

--- spellingnumbs3.py
+++ spellingnumbs3.py
@@ -1,3 +1,14 @@
+'''
+need to fix output formatting
+need to add quit option
+need to add handling for nengative numbers
+need to floats (?)
+'''
+
+
+
+
+
 print('''
 
 
@@ -7,7 +18,8 @@ print('''
  █   ███   ██  ██   ██ ██ ██   ██    █ ██       ██        ▄     ▀██  ██    █ ██       ██   ██  ██       ██     
 ▄█▄   ▀█   ▀█▄▄▀█▄ ▄██ ██ ██▄  ▀█▄▄▄▀   ▀█▄▄▄▀ ▄██▄       █▀▄▄▄▄█▀   ██▄▄▄▀   ▀█▄▄▄▀ ▄██▄ ▄██▄  ▀█▄▄▄▀ ▄██▄    
                                                                      ██                                        
-                                                                    ▀▀▀▀                                       
+                                                                    ▀▀▀▀  
+                                    Enter 'quit' to exit                                      
 
 ''')
 
@@ -58,9 +70,9 @@ def xteen(i, placevalue):
 
 
     if placevalue == 'none':
-        print(xteenvalue, end=" ")
+        print(xteenvalue, end="")
     else:
-        print(xteenvalue + placevalue, end=" ")
+        print(xteenvalue + placevalue, end="")
     
 
 
@@ -88,9 +100,9 @@ def singledigitfunction(i, placevalue):
         val = 'Nine'
    
     if placevalue == 'none':
-        print(val)
+        print(val, end="")
     else:
-        print(val + placevalue, end= "")   
+        print(val  +placevalue, end="")   
 
 def doubledigitfunction(i, placevalue):
     val = '0'
@@ -116,14 +128,13 @@ def doubledigitfunction(i, placevalue):
         val = 'Ninety'
    
     if placevalue == 'none':
-        print(val)
+        print(val, end=" ")
     else:
-        print(val + placevalue, end= "")  
-
+        print(val + placevalue, end="")  
 
 while True:
     try:
-        user_input_number = int(input('Enter a number between 0 and 1000000: '))
+        user_input_number = int(input(' Enter a number between 0 and 1000000: '))
         break
     except:
         print('Please enter a valid integer between 0 and 1000000')
@@ -134,7 +145,7 @@ user_input_number = user_input_number.zfill(7)
 numlist = (user_input_number[0], user_input_number[1], user_input_number[2],
 user_input_number[3],user_input_number[4],user_input_number[5],user_input_number[6])
 
-print(numlist)
+#print(numlist) Uncomment to show integer split up into placevalues
 
 if len(numlist) == 7:
     placevalue['Millions'] = numlist[0]
@@ -157,8 +168,10 @@ for i in placevalue: #millions
 
 for i in placevalue: #hundred thousands
 
-    if placevalue['Hundred-Thousands'] != '0' and placevalue['Ten-Thousands'] == '0': 
+    if placevalue['Hundred-Thousands'] != '0' and placevalue['Ten-Thousands'] == '0' and placevalue['Thousands'] != '0': 
         singledigitfunction(placevalue['Hundred-Thousands'], ' Hundred-and ')
+    elif placevalue['Hundred-Thousands'] != '0' and placevalue['Ten-Thousands'] == '0' and placevalue['Thousands'] == '0':
+        singledigitfunction(placevalue['Hundred-Thousands'], ' Hundred-Thousand ')
     elif placevalue['Hundred-Thousands'] != '0' and placevalue['Ten-Thousands'] != '0':
         singledigitfunction(placevalue['Hundred-Thousands'], ' Hundred ')
     elif placevalue['Hundred-Thousands'] == '0':
@@ -179,12 +192,6 @@ for i in placevalue: #ten thousands
             doubledigitfunction(placevalue['Ten-Thousands'], ' Thousand ') 
         elif placevalue['Ten-Thousands'] > '1' and placevalue['Thousands'] >= '0':
             doubledigitfunction(placevalue['Ten-Thousands'], 'none') #ex: "fifty" thousand
-
-
-
-        #doubledigitfunction(placevalue['Ten-Thousands'], 'none') #ex: "fifty" thousand
-    #elif placevalue['Ten-Thousands'] == '1' and placevalue['Thousands'] > '0': 
-        #xteen(placevalue['Thousands'], ' Thousand ') #sep function needed to definte 11 - 19 thousand
         elif placevalue['Ten-Thousands'] == '0':
             break 
         break
@@ -237,63 +244,5 @@ for i in placevalue: #ones
 
 
 
-'''
-if len(numlist) == 7:
-    placevalue['Millions'] = numlist[0]
-    placevalue['Hundred-Thousands'] = numlist[1]
-    placevalue['Ten-Thousands'] = numlist[2]
-    placevalue['Thousands'] = numlist[3]
-    placevalue['Hundreds'] = numlist[4]
-    placevalue['Tens'] = numlist[5]
-    placevalue['Ones'] = numlist[6]
-elif len(numlist) == 6:
-    placevalue['Millions'] = listplaceholder
-    placevalue['Hundred-Thousands'] = numlist[0]
-    placevalue['Ten-Thousands'] = numlist[1]
-    placevalue['Thousands'] = numlist[2]
-    placevalue['Hundreds'] = numlist[3]
-    placevalue['Tens'] = numlist[4]
-    placevalue['Ones'] = numlist[5]
-    
-elif len(numlist) == 5:
-    placevalue['Millions'] = listplaceholder
-    placevalue['Hundred-Thousands'] = listplaceholder
-    placevalue['Ten-Thousands'] = numlist[0]
-    placevalue['Thousands'] = numlist[1]
-    placevalue['Hundreds'] = numlist[2]
-    placevalue['Tens'] = numlist[3]
-    placevalue['Ones'] = numlist[4]
-elif len(numlist) == 4:
-    placevalue['Millions'] = listplaceholder
-    placevalue['Hundred-Thousands'] = listplaceholder
-    placevalue['Ten-Thousands'] = listplaceholder
-    placevalue['Thousands'] = numlist[0]
-    placevalue['Hundreds'] = numlist[1]
-    placevalue['Tens'] = numlist[2]
-    placevalue['Ones'] = numlist[3]
-elif len(numlist) == 3:
-    placevalue['Millions'] = listplaceholder
-    placevalue['Hundred-Thousands'] = listplaceholder
-    placevalue['Ten-Thousands'] = listplaceholder
-    placevalue['Thousands'] = listplaceholder
-    placevalue['Hundreds'] = numlist[0]
-    placevalue['Tens'] = numlist[1]
-    placevalue['Ones'] = numlist[2]
-elif len(numlist) == 2:
-    placevalue['Millions'] = listplaceholder
-    placevalue['Hundred-Thousands'] = listplaceholder
-    placevalue['Ten-Thousands'] = listplaceholder
-    placevalue['Thousands'] = listplaceholder
-    placevalue['Hundreds'] = listplaceholder
-    placevalue['Tens'] = numlist[0]
-    placevalue['Ones'] = numlist[1]
-elif len(numlist) == 1:
-    placevalue['Millions'] = listplaceholder
-    placevalue['Hundred-Thousands'] = listplaceholder
-    placevalue['Ten-Thousands'] = listplaceholder
-    placevalue['Thousands'] = listplaceholder
-    placevalue['Hundreds'] = listplaceholder
-    placevalue['Tens'] = listplaceholder
-    placevalue['Ones'] = numlist[0]
-'''
+
 
