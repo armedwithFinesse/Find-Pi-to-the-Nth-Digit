cb69d344aafd08b1a8c0373203a5060e3e5903ca
Ziyad <zdn313@yahoo.com>
2022-08-23 17:47:25 -0500

    board visual proper, non interactive

--- chess.py
+++ chess.py
@@ -1,21 +1,24 @@
 board = [['.  .  .  .  .  .  .  .'],['P  P  P  P  P  P  P  P'],['R  N  B  K  Q  B  N  R']]
-notation = ['  A  B  C  D  E  F  G  H']  
+notation = ['A  B  C  D  E  F  G  H']  
 notation_number = ['1', '2', '3', '4', '5', '6', '7','8']     
 
+for number in notation_number:
+    notation_number.reverse()
+    if number != '1' and number != '2' and number != '7' and number != '8':
+        print(notation_number[int(number) - 1],' ',board[0])
+    elif number == '2' or number == '7':
+        print(notation_number[int(number) - 1],' ',board[1])
+    elif number == '1' or number == '8':
+        print(notation_number[int(number) - 1],' ',board[2])
+
+print()
+print('   ' , notation)
 
 
-print(notation)
 
-for i in range(1):
-    print(notation_number[i],board[2])
-for i in range(1):
-    print(notation_number[i],board[1])
-for i in range(5):
-    print(notation_number[i],board[0])
-for i in range(1):
-    print(notation_number[i],board[1])
-for i in range(1):
-    print(notation_number[i],board[2])
+
+
+
 
 
 
