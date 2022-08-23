a519b0a147eb419abd9ce5e2ca435b7a21a92df8
Ziyad <zdn313@yahoo.com>
2022-08-23 16:50:21 -0500

    prelim ideas

---
+++ chess.py
@@ -0,0 +1,21 @@
+board = [['.  .  .  .  .  .  .  .'],['P  P  P  P  P  P  P  P'],['R  N  B  K  Q  B  N  R']]
+notation = ['  A  B  C  D  E  F  G  H']  
+notation_number = ['1', '2', '3', '4', '5', '6', '7','8']     
+
+
+
+print(notation)
+
+for i in range(1):
+    print(notation_number[i],board[2])
+for i in range(1):
+    print(notation_number[i],board[1])
+for i in range(5):
+    print(notation_number[i],board[0])
+for i in range(1):
+    print(notation_number[i],board[1])
+for i in range(1):
+    print(notation_number[i],board[2])
+
+
+
