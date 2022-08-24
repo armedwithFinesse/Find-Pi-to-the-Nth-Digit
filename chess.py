55faf6bd0577a72878cc05758c5eac37cbfa9a19
Ziyad <zdn313@yahoo.com>
2022-08-23 21:58:10 -0500

    cleaned up code

--- chess.py
+++ chess.py
@@ -1,22 +1,7 @@
-#board = [['.  .  .  .  .  .  .  .'],['P  P  P  P  P  P  P  P'],['R  N  B  K  Q  B  N  R']]
 board = [['.', '.', '.', '.', '.', '.', '.', '.'],['P','P','P','P','P','P','P','P',],['R',  'N', 'B',  'K',  'Q',  'B',  'N',  'R']]
 
 notation_letter = '    A  B  C  D  E  F  G  H'
 notation_number = ['1', '2', '3', '4', '5', '6', '7','8']     
-
-
-'''for number in notation_number:
-    notation_number.reverse()
-    if number != '1' and number != '2' and number != '7' and number != '8':
-        print(notation_number[int(number) - 1],' ',board[0])
-    elif number == '2' or number == '7':
-        print(notation_number[int(number) - 1],' ',board[1])
-    elif number == '1' or number == '8':
-        print(notation_number[int(number) - 1],' ',board[2])
-
-print()
-print('   ' , notation_letter)'''
-
 
 
 notation_dict_row_eight = {'A8':board[2][0],'B8':board[2][1],'C8':board[2][2],'D8':board[2][3],'E8':board[2][4],'F8':board[2][5],'G8':board[2][6],'H8':board[2][7]}
@@ -30,53 +15,45 @@ notation_dict_row_one = {'A1':board[2][0],'B1':board[2][1],'C1':board[2][2],'D1'
 
 notation_list = [notation_dict_row_eight, notation_dict_row_seven, notation_dict_row_six, notation_dict_row_five, notation_dict_row_four, notation_dict_row_three, notation_dict_row_two, notation_dict_row_one]
 
-'''for row in notation_list:
-    while row == notation_list[0]:
-        print(notation_list[0].values())
-        row = None
-    while row == notation_list[1]:
-        print(notation_list[1].values())
-        row = None
-    while row == notation_list[2]:
-        print(notation_list[2].values())
-        row = None
-    while row == notation_list[3]:
-        print(notation_list[3].values())
-        row = None
-    while row == notation_list[4]:
-        print(notation_list[4].values())
-        row = None
-    while row == notation_list[5]:
-        print(notation_list[5].values())
-        row = None
-    while row == notation_list[6]:
-        print(notation_list[6].values())
-        row = None
-    while row == notation_list[7]:
-        print(notation_list[7].values())
-        row = None'''
-    
-    
 
+    
 def row_maker(row,notation_number):
     not_num = 0
     emptystr = ""
     for item in row:
         not_num + 1
-        emptystr += row[item] + '  '
-    #print(str(not_num) + emptystr)
-    
+        emptystr += row[item] + '  '    
     return emptystr
     
-
-count = 8
-for row in notation_list:
-    x = row_maker(row, notation_number)
-    print(str(count) + '  ', x)
-    count -= 1
     
+def notation(notation_list,notation_number):
+    count = 8
+    for row in notation_list:
+        x = row_maker(row, notation_number)
+        print(str(count) + '  ', x)
+        count -= 1
+    print('\n' + notation_letter)
+
+
+def give_board():
+    notation(notation_list, notation_number)
 
 
 
 
-print('\n' + notation_letter)
+give_board()
+#====================================================================================NOW BEGIN TAKING USER INPUT
+while True:
+    player_one = input('Player 1: ')
+    for key in notation_dict_row_two.keys():
+        try: 
+            if player_one == key:
+                break
+        except:
+            print('Move invalid. Try again.')
+            break
+
+
+
+
+Player_two = input('Player 2:  ')
